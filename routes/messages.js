f862bc56f5f064a11d8e6cd742b6d6040b6f923a
Ephraim Gbadebo <epgbadebo@atlanta.k12.ga.us>
2022-02-24 16:38:29 -0500

    first commit

---
+++ routes/messages.js
@@ -0,0 +1,70 @@
+const jwt = require("jsonwebtoken");
+const Router = require("express").Router;
+const router = new Router();
+const Message = require("../models/message");
+const User = require("../models/user");
+
+const { SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+
+/** GET /:id - get detail of message.
+ *
+ * => {message: {id,
+ *               body,
+ *               sent_at,
+ *               read_at,
+ *               from_user: {username, first_name, last_name, phone},
+ *               to_user: {username, first_name, last_name, phone}}
+ *
+ * Make sure that the currently-logged-in users is either the to or from user.
+ *
+ **/
+router.get('/:id', ensureCorrectUser, async function(req, res, next) {
+    try {
+        let message = await Message.get(req.params.id);
+
+        return res.json({message});
+    }
+    catch(err) {
+        return next(err);
+    }
+});
+
+
+/** POST / - post message.
+ *
+ * {to_username, body} =>
+ *   {message: {id, from_username, to_username, body, sent_at}}
+ *
+ **/
+router.post('/', ensureCorrectUser, async function(req, res, next) {
+    try {
+        let { from_username, to_username, body } = req.body;
+        let message = await Message.create({from_username, to_username, body});
+
+        return res.json({message});
+    }
+    catch(err) {
+        return next(err);
+    }
+});
+
+/** POST/:id/read - mark message as read:
+ *
+ *  => {message: {id, read_at}}
+ *
+ * Make sure that the only the intended recipient can mark as read.
+ *
+ **/
+router.post('/:id/read', ensureCorrectUser, async function(req, res, next) {
+    try {
+        let message = await Message.markRead(req.params.id);
+
+        return res.json({message});
+    }
+    catch(err) {
+        return next(err);
+    }
+});
+
